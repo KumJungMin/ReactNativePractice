e7ec77a3d8dd6aae38b417e08f093de531fb23ed
KumJungMin <rmawjdals@naver.com>
2024-05-25 15:08:45 +0900

    4. location 사용하는 법

--- weather/App.js
+++ weather/App.js
@@ -1,25 +1,96 @@
-import { StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync({
+        accuracy: 5,
+      });
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity(location[0].city);
+    })();
+  }, []);
+
   return (
-    // View의 기본값은 Flex Container이다.
     <View style={styles.container}>
-      <View style={styles.item}></View>
-      <View style={styles.item}></View>
-      <View style={styles.item}></View>
+      <StatusBar style="light" />
+
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      {/* TIP: ScrollView 스타일은 style아 이난 contentContainerStyle로 지정해야함*/}
+      {/* pagingEnabled: 스크롤을 한 페이지씩 이동하게 해줌 */}
+      {/* showsHorizontalScrollIndicator: 스크롤바를 보여줄지 말지 결정 */}
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Clouds</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // 사이즈를 조절할 떄 px이 아닌 flex를 사용한다.
-  // -> 다양한 화면 크기에 대응하기 위함이
   container: {
     flex: 1,
+    backgroundColor: "pink",
   },
-  item: {
-    flex: 1,
-    backgroundColor: "red",
-    borderWidth: 2,
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    // backgroundColor: "pink",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
