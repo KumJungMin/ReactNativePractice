d2eda59789a4baa9fe78a6d70ebfac9d108d166c
KumJungMin <rmawjdals@naver.com>
2024-05-25 14:37:20 +0900

    3. layout 구성하는 법

--- weather/App.js
+++ weather/App.js
@@ -2,7 +2,8 @@ import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    // View의 기본값은 Flex Container이다.
+    <View style={styles.container}>
       <View style={styles.item}></View>
       <View style={styles.item}></View>
       <View style={styles.item}></View>
@@ -11,9 +12,13 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  // 사이즈를 조절할 떄 px이 아닌 flex를 사용한다.
+  // -> 다양한 화면 크기에 대응하기 위함이
+  container: {
+    flex: 1,
+  },
   item: {
-    width: 200,
-    height: 200,
+    flex: 1,
     backgroundColor: "red",
     borderWidth: 2,
   },
