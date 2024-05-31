a51694a3c940968f93a6554241f5d34f27791ad1
KumJungMin <rmawjdals@naver.com>
2024-05-31 15:11:29 +0900

    input 이벤트에는 e.currentTarget.value가 아닌 값이 바로 전송됨

--- front/App.tsx
+++ front/App.tsx
@@ -6,14 +6,24 @@
  */
 
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
 
 function App(): React.JSX.Element {
+  const [name, setName] = React.useState('');
+
+  // react와 달리 이벤트에 e.target.value를 사용하지 않고 text를 사용
+  const handleChangeInput = (text: string) => {
+    setName(text);
+  };
+
   // SafeAreaView: 노치 디자인이나 화면의 경계를 벗어나는 요소를 자동으로 제거해주는 컴포넌트
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Test</Text>
-      <Button title="Press me" onPress={() => console.log('pressed')} />
+      <Text> 이름: {name}</Text>
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={handleChangeInput}
+      />
     </SafeAreaView>
   );
 }
