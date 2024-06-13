6234f174f5e7ee126bc80967b25fbafeaecc3462
KumJungMin <rmawjdals@naver.com>
2024-06-13 21:27:40 +0900

    탭 구성하기

--- video/app/(tabs)/_layout.tsx
+++ video/app/(tabs)/_layout.tsx
@@ -1,13 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import { Tabs, Redirect, SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
-/** (tabs): 하단 네비게이션 탭 역할 */ 
 
-const TabsLayout = () => {
+interface TabIconProps {
+  color: string,
+  name: string,
+}
+
+// SplashScreen.preventAutoHideAsync: 스플래시 화면을 숨기지 않도록 설정
+SplashScreen.preventAutoHideAsync();
+
+const TabIcon = ({ name, color }: TabIconProps) => {
   return (
     <View>
-      <Text>_layout</Text>
+      <Text style={{ color }}>
+        { name }
+      </Text>
     </View>
+  );
+};
+
+/** (tabs): 하단 네비게이션 탭 역할 */ 
+const TabsLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "SpaceMono-Regular": require("../../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  // fontsLoaded: 폰트 로딩이 완료되면 true, 아니면 false
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) {
+      // SplashScreen.hideAsync: 스플래시 화면을 숨김
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen 
+          name="home" 
+          options={{ 
+            title: '홈' ,
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <TabIcon color={color} name='H' />
+            )
+          }} 
+          />
+      </Tabs>
+    </>
   )
 }
 
--- video/app/index.tsx
+++ video/app/index.tsx
@@ -1,18 +1,26 @@
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-  <SafeAreaView>
+  <SafeAreaView style={styles.container}>
     <Text style={styles.font}>
       안녕!
     </Text>
+    <Link href="/home">Home</Link>
   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   font: {
     fontSize: 20,
-    color: 'white',
+    color: 'blue',
   },
 });
